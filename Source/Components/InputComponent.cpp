e6065d36500e045d73431ce772b42c4a5f35e3f6
carlberyeur <carlcarlberger@gmail.com>
2017-02-15 21:16:04 +0100

    added something im gonna try out but i'm going home now so commenting it out

--- Source/Components/InputComponent.cpp
+++ Source/Components/InputComponent.cpp
@@ -8,6 +8,53 @@ CInputComponent::CInputComponent()
 
 CInputComponent::~CInputComponent()
 {
+	// if (CommonUtilities::InputWrapper::GetKeyDown(DIK_D) == true && myVelocity.x > -myMaxSpeed)
+	// {
+		// myVelocity.x += myAcceleration * aDeltaTime.GetSeconds();
+	// }
+	// else if (CommonUtilities::InputWrapper::GetKeyDown(DIK_A) == true && myVelocity.x < myMaxSpeed)
+	// {
+		// myVelocity.x -= myAcceleration * aDeltaTime.GetSeconds();
+	// }
+	// else
+	// {
+		// if (myVelocity.x > myDeceleration * aDeltaTime.GetSeconds())
+		// {
+			// myVelocity.x -= myDeceleration * aDeltaTime.GetSeconds();
+		// }
+		// else if (myVelocity.x < -myDeceleration * aDeltaTime.GetSeconds())
+		// {
+			// myVelocity.x += myDeceleration * aDeltaTime.GetSeconds();
+		// }
+		// else
+		// {
+			// myVelocity.x = 0;
+		// }
+	// }
+
+	// if (CommonUtilities::InputWrapper::GetKeyDown(DIK_S) == true && myVelocity.y > -myMaxSpeed)
+	// {
+		// myVelocity.y += myAcceleration * aDeltaTime.GetSeconds();
+	// }
+	// else if (CommonUtilities::InputWrapper::GetKeyDown(DIK_W) == true && myVelocity.y < myMaxSpeed)
+	// {
+		// myVelocity.y -= myAcceleration * aDeltaTime.GetSeconds();
+	// }
+	// else
+	// {
+		// if (myVelocity.y > myDeceleration * aDeltaTime.GetSeconds())
+		// {
+			// myVelocity.y -= myDeceleration * aDeltaTime.GetSeconds();
+		// }
+		// else if (myVelocity.y < -myDeceleration * aDeltaTime.GetSeconds())
+		// {
+			// myVelocity.y += myDeceleration * aDeltaTime.GetSeconds();
+		// }
+		// else
+		// {
+			// myVelocity.y = 0;
+		// }
+	// }
 }
 
 void CInputComponent::Receive(const eComponentMessageType /*aMessageType*/, const SComponentMessageData& /*aMessageData*/)
