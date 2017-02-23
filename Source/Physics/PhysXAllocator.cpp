2e34ac13bd8bf41b75f1bb96f5c73fce3021cf0d
Edvin Jacobsson Pontén <resanderum@gmail.com>
2017-02-23 17:40:15 +0100

    Physx Allocator

--- Source/Physics/PhysXAllocator.cpp
+++ Source/Physics/PhysXAllocator.cpp
@@ -2,24 +2,26 @@
 #include "PhysXAllocator.h"
 #include "foundation/PxAssert.h"
 
-
-CPhysXAllocator::CPhysXAllocator()
+namespace Physics
 {
-}
+	CPhysXAllocator::CPhysXAllocator()
+	{
+	}
 
 
-CPhysXAllocator::~CPhysXAllocator()
-{
-}
+	CPhysXAllocator::~CPhysXAllocator()
+	{
+	}
 
-void * CPhysXAllocator::allocate(size_t size, const char * typeName, const char * filename, int line)
-{
-	void* ptr = platformAlignedAlloc(size);
-	PX_ASSERT((reinterpret_cast<size_t>(ptr) & 15) == 0);
-	return ptr;
-}
+	void * CPhysXAllocator::allocate(size_t size, const char * typeName, const char * filename, int line)
+	{
+		void* ptr = platformAlignedAlloc(size);
+		PX_ASSERT((reinterpret_cast<size_t>(ptr) & 15) == 0);
+		return ptr;
+	}
 
-void CPhysXAllocator::deallocate(void * ptr)
-{
-	platformAlignedFree(ptr);
+	void CPhysXAllocator::deallocate(void * ptr)
+	{
+		platformAlignedFree(ptr);
+	}
 }
