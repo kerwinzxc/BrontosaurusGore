6bdbee32044df04ff9005c2eb4a617cd03e7b1f6
Edvin Jacobsson Pontén <resanderum@gmail.com>
2017-02-23 17:41:13 +0100

    physx Allocator

--- Source/Physics/PhysXAllocator.h
+++ Source/Physics/PhysXAllocator.h
@@ -4,57 +4,54 @@
 #include <stdlib.h>
 #include <malloc.h>
 
-
-
-class CPhysXAllocator : public physx::PxAllocatorCallback
+namespace Physics
 {
-public:
-	CPhysXAllocator();
-	virtual ~CPhysXAllocator();
-
-
-
-	/**
-	\brief Allocates size bytes of memory, which must be 16-byte aligned.
-
-	This method should never return NULL.  If you run out of memory, then
-	you should terminate the app or take some other appropriate action.
-
-	<b>Threading:</b> This function should be thread safe as it can be called in the context of the user thread
-	and physics processing thread(s).
-
-	\param size			Number of bytes to allocate.
-	\param typeName		Name of the datatype that is being allocated
-	\param filename		The source file which allocated the memory
-	\param line			The source line which allocated the memory
-	\return				The allocated block of memory.
-	*/
-	virtual void* allocate(size_t size, const char* typeName, const char* filename, int line);
-	/**
-	\brief Frees memory previously allocated by allocate().
-
-	<b>Threading:</b> This function should be thread safe as it can be called in the context of the user thread
-	and physics processing thread(s).
-
-	\param ptr Memory to free.
-	*/
-	virtual void deallocate(void* ptr) override;
-
-
-
-private:
-
-	// on win32 we only have 8-byte alignment guaranteed, but the CRT provides special aligned allocation fns
-	PX_FORCE_INLINE void* platformAlignedAlloc(size_t size)
+	class CPhysXAllocator : public physx::PxAllocatorCallback
 	{
-		return _aligned_malloc(size, 16);
-	}
+	public:
+		CPhysXAllocator();
+		virtual ~CPhysXAllocator();
 
-	PX_FORCE_INLINE void platformAlignedFree(void* ptr)
-	{
-		_aligned_free(ptr);
-	}
+		/**
+		\brief Allocates size bytes of memory, which must be 16-byte aligned.
 
-};
+		This method should never return NULL.  If you run out of memory, then
+		you should terminate the app or take some other appropriate action.
 
+		<b>Threading:</b> This function should be thread safe as it can be called in the context of the user thread
+		and physics processing thread(s).
+
+		\param size			Number of bytes to allocate.
+		\param typeName		Name of the datatype that is being allocated
+		\param filename		The source file which allocated the memory
+		\param line			The source line which allocated the memory
+		\return				The allocated block of memory.
+		*/
+		virtual void* allocate(size_t size, const char* typeName, const char* filename, int line);
+		/**
+		\brief Frees memory previously allocated by allocate().
+
+		<b>Threading:</b> This function should be thread safe as it can be called in the context of the user thread
+		and physics processing thread(s).
+
+		\param ptr Memory to free.
+		*/
+		virtual void deallocate(void* ptr) override;
+
+
+
+	private:
+
+		// on win32 we only have 8-byte alignment guaranteed, but the CRT provides special aligned allocation fns
+		PX_FORCE_INLINE void* platformAlignedAlloc(size_t size)
+		{
+			return _aligned_malloc(size, 16);
+		}
+
+		PX_FORCE_INLINE void platformAlignedFree(void* ptr)
+		{
+			_aligned_free(ptr);
+		}
+	};
+}
 
