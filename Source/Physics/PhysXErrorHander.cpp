22f8069d4d6288ce9738a08ff7185a8a1985402c
Edvin Jacobsson Pontén <resanderum@gmail.com>
2017-02-23 17:41:01 +0100

    physx error handler :pineapple:

--- Source/Physics/PhysXErrorHander.cpp
+++ Source/Physics/PhysXErrorHander.cpp
@@ -2,17 +2,21 @@
 #include "PhysXErrorHander.h"
 
 
-CPhysXErrorHander::CPhysXErrorHander()
+
+namespace Physics
 {
-}
+	CPhysXErrorHander::CPhysXErrorHander()
+	{
+	}
 
 
-CPhysXErrorHander::~CPhysXErrorHander()
-{
-}
+	CPhysXErrorHander::~CPhysXErrorHander()
+	{
+	}
 
-void CPhysXErrorHander::reportError(physx::PxErrorCode::Enum code, const char* message, const char* file, int line)
-{
-	PHYSICS_LOG("Error %i: \n\t %s \n\t File: %s Line: %i \n\n", code, message, file, line);
-	DL_ASSERT("");
+	void CPhysXErrorHander::reportError(physx::PxErrorCode::Enum code, const char* message, const char* file, int line)
+	{
+		PHYSICS_LOG("\nError %i: \n\t %s \n\t File: %s Line: %i \n\n", code, message, file, line);
+		DL_ASSERT("");
+	}
 }
