5dd8e1bd1344d8716a1af2aa0ffe87331e231036
MarcusM <marcus.malmqvist.sp15@thegameassembly.com>
2017-02-28 17:59:51 +0100

    Fixed weird shoot bugg. Talk to me if you want to change this back plz.

--- Source/Components/ModelComponent.cpp
+++ Source/Components/ModelComponent.cpp
@@ -34,7 +34,7 @@ CU::Matrix44f CModelComponent::GetToWorldTransform()
 {
 	if (GetParent() != nullptr)
 	{
-		return GetParent()->GetToWorldTransform() * CU::Matrix44f::CreateRotateAroundY(PI);
+		return GetParent()->GetToWorldTransform()/* * CU::Matrix44f::CreateRotateAroundY(PI)*/;
 	}
 	else
 	{
