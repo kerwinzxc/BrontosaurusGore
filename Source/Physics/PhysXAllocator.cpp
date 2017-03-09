447d58df27f57b249ba847bb69ccc1affb0023df
Edvin Jacobsson Pontén <resanderum@gmail.com>
2017-03-09 21:10:51 +0100

    comment

--- Source/Physics/PhysXAllocator.cpp
+++ Source/Physics/PhysXAllocator.cpp
@@ -16,7 +16,7 @@ namespace Physics
 	void * CPhysXAllocator::allocate(size_t size, const char * typeName, const char * filename, int line)
 	{
 		void* ptr = platformAlignedAlloc(size);
-		//PHYSICS_LOG()
+		//PHYSICS_LOG("%s (%i Bytes) allocated at line: %i, in file: %s ", typeName, (int)size, line, filename);
 		PX_ASSERT((reinterpret_cast<size_t>(ptr) & 15) == 0);
 		return ptr;
 	}
