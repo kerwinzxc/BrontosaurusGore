51b803f77ec3df31a8716b6937fff57e49899900
Edvin Jacobsson Pontén <resanderum@gmail.com>
2017-03-09 21:09:38 +0100

    new classes, PhysicsActor, PhysicsActorStatic & PhysicsActorDynamic :pineapple:

---
+++ Source/Physics/PhysicsActorStatic.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "PhysicsActor.h"
+
+namespace physx
+{
+	class PxRigidStatic;
+}
+
+namespace Physics
+{
+	class CPhysicsActorStatic : public CPhysicsActor
+	{
+	public:
+		CPhysicsActorStatic(physx::PxRigidStatic* aActor, CShape* aShape);
+		~CPhysicsActorStatic();
+		inline const EActorType GetType() const override { return EActorType::eStatic; }
+
+	private:
+		physx::PxRigidStatic* myStaticActor;
+	};
+}
