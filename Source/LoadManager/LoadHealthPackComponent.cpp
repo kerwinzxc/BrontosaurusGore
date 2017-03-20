ee0cc529bd402e6dffb0193a7c1e7ba34e284b3e
Johan <johan.hagelqvist.sp15@thegameassembly.com>
2017-03-20 15:57:42 +0100

    liker errors are now not retarded (at least these ones)

--- Source/LoadManager/LoadHealthPackComponent.cpp
+++ Source/LoadManager/LoadHealthPackComponent.cpp
@@ -1,6 +1,6 @@
 #include "stdafx.h"
 #include "LoadHealthPackComponent.h"
-#include "HealthPackComponent.h"
+#include "HealthPickupComponent.h"
 #include "PickupComponentManager.h"
 #include "ComponentManager.h"
 
@@ -8,7 +8,7 @@ int LoadHealthPackComponent(KLoader::SLoadedComponentData someData)
 {
 	static int id = 0;
 	healthPoint loadedHealthValue = static_cast<healthPoint>(someData.myData.at("ReplenishAmount").GetInt());
-	CHealthPackComponent* healthPackComponent = CPickupComponentManager::GetInstance()->CreateAndRegisterHealthPickupComponent(id++, loadedHealthValue);
+	CHealthPickupComponent* healthPackComponent = CPickupComponentManager::GetInstance()->CreateAndRegisterHealthPickupComponent(id++, loadedHealthValue);
 	return healthPackComponent->GetId();
 }
 
