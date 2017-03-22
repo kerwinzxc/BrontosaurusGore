73db9ef52377c2c96be96d14c16f8062a21de4d4
Kyle <kyle.thorburn.sp15@thegameassembly.com>
2017-03-22 16:13:58 +0100

    major refactor of enemy

--- Source/Components/Enemy.cpp
+++ Source/Components/Enemy.cpp
@@ -1,79 +1,68 @@
 #include "stdafx.h"
 #include "Enemy.h"
 #include "../TServer/ServerMessageManager.h"
-#include "../TShared/NetworkMessage_EnemyPosition.h"
+#include "../TShared/NetworkMessage_EnemyTransformation.h"
 #include "../ThreadedPostmaster/Postmaster.h"
 #include "../ThreadedPostmaster/SendNetowrkMessageMessage.h"
-#include "PollingStation.h"
 
-//CU::GrowingArray<CGameObject*> Component::CEnemy::myPlayerObject;
-CGameObject* Component::CEnemy::myPlayerObj;
+CU::GrowingArray<CGameObject*> Component::CEnemy::ourPlayerObjects;
 
-Component::CEnemy::CEnemy(unsigned int anId): myHealth(0), mySpeed(0), myDetectionRange2(0), myStartAttackRange2(0), myStopAttackRange2(0), myServerId(anId)
+Component::CEnemy::CEnemy(unsigned int aId): myHealth(0), mySpeed(0), myDetectionRange2(0), myStartAttackRange2(0), myStopAttackRange2(0)
 {
 	myIsDead = false;
+	myServerId = aId;
 }
-
 
 Component::CEnemy::~CEnemy()
 {
 }
 
-void Component::CEnemy::Move(const CU::Vector3f& aDispl)
+void Component::CEnemy::UpdateTransformation()
 {
+	const CU::Matrix44f tranformation = GetParent()->GetLocalTransform();
+	CNetworkMessage_EnemyTransformation* message = CServerMessageManager::GetInstance()->CreateMessage<CNetworkMessage_EnemyTransformation>(ID_ALL_BUT_ME);
+	message->SetId(myServerId);
+	message->SetTransformation(tranformation);
 
-	GetParent()->GetLocalTransform().Move(aDispl);
-	const CU::Vector3f pos = GetParent()->GetWorldPosition();
-	//CNetworkMessage_EnemyPosition* message = CServerMessageManager::GetInstance()->CreateMessage<CNetworkMessage_EnemyPosition>(ID_ALL_BUT_ME);
-	//message->SetId(myServerId);
-	// message->SetPosition(pos);
+	Postmaster::Threaded::CPostmaster::GetInstance().Broadcast(new CSendNetowrkMessageMessage(message));
+}
 
-	///Postmaster::Threaded::CPostmaster::GetInstance().Broadcast(new CSendNetowrkMessageMessage(message));
+void Component::CEnemy::MoveForward(const float aMovAmount)
+{
+	GetParent()->GetLocalTransform().Move({0.f, 0.f, aMovAmount});
 }
 
 void Component::CEnemy::Update(const CU::Time& aDeltaTime)
 {
-	CGameObject* coolo = CPollingStation::GetInstance()->GetPlayerObject();
-	if (myPlayerObj != nullptr)
+
+	if (myIsDead == false)
 	{
-		CU::Vector3f playerPosition = myPlayerObj->GetWorldPosition();
+		bool hasChanged = false;
+		const CU::Vector3f closestPlayerPos = ClosestPlayerPosition();
+		const CU::Vector3f myPos = GetParent()->GetWorldPosition();
+		const CU::Vector3f toPlayer = closestPlayerPos - myPos;
+		const float distToPlayer = toPlayer.Length2();
 
-		GetParent()->GetLocalTransform().LookAt(playerPosition);
-		GetParent()->NotifyComponents(eComponentMessageType::eMoving, SComponentMessageData());
+
+		if (distToPlayer < myDetectionRange2)
+		{
+			hasChanged = true;
+
+			GetParent()->Face(toPlayer);
+		}
+		if (distToPlayer < myStartAttackRange2)
+		{
+			// impl attack code.
+		}
+		else if (distToPlayer < myDetectionRange2)
+		{
+			float movementAmount = mySpeed * aDeltaTime.GetSeconds();
+			MoveForward(movementAmount);
+		}
 
 
-		//if (myIsDead == false)
-			//{
-
-			//	const CU::Vector3f playerPosition = CPollingStation::GetInstance()->GetPlayerObject()->GetWorldPosition();
-			//	const CU::Vector3f position = GetParent()->GetWorldPosition();
-			//	const CU::Vector3f toPlayer = playerPosition - position;
-			//	const float distToPlayer = toPlayer.Length2();
-
-			//	if (distToPlayer < myDetectionRange2)
-			//	{
-			//		//GetParent()->Face(toPlayer);
-			//		GetParent()->GetLocalTransform().LookAt(playerPosition);
-			//		
-
-			//	}
-			//	if (distToPlayer < myStartAttackRange2)
-			//	{
-
-			//	}
-			//	else if (distToPlayer < myDetectionRange2)
-			//	{
-			//		float movementAmount = mySpeed * aDeltaTime.GetSeconds();
-			//		//CU::Vector3f displacement = toPlayer.GetNormalized() * movementAmount;
-			//		//Move(displacement);
-
-			//		//if (GetParent()->AskComponents(eComponentQuestionType::eMovePhysicsController, data) == true)
-			//		//{
-			//			Move({ 0.f, 0.f, movementAmount });
-			//		//}
-			//	}
-
-			//}
+		if(hasChanged == true)
+			UpdateTransformation();
 	}
 }
 
@@ -87,38 +76,33 @@ void Component::CEnemy::Receive(const eComponentMessageType aMessageType, const 
 	}
 }
 
-void Component::CEnemy::SetPlayer(CGameObject* aPlayerObject)
+void Component::CEnemy::SetPlayerObject(CGameObject* aPlayerObj)
 {
-	myPlayerObj = aPlayerObject; //sätts genom server också tydligen ???
-	DL_PRINT("PlayerObj set in enemy.");
-	//if (myPlayerObject.IsInitialized() == false)
-	//{
-	//	myPlayerObject.Init(2);
-	//}
-	//myPlayerObject.Add(playerObject);
+	if (ourPlayerObjects.IsInitialized() == false)
+		ourPlayerObjects.Init(4);
+	ourPlayerObjects.Add(aPlayerObj);
 }
 
 CU::Vector3f Component::CEnemy::ClosestPlayerPosition()
 {
-	//const CU::Vector3f position = GetParent()->GetWorldPosition();
-	//
-	//if(myPlayerObject.IsInitialized() == false || myPlayerObject.Size() == 0)
-	//{
-	//	return position;
-	//}
-	//
-	//CU::Vector3f playerPos = myPlayerObject[0]->GetWorldPosition();
-	//for(int i = 0; i < myPlayerObject.Size(); ++i)
-	//{
-	//	CGameObject*const player = myPlayerObject[i];
-	//	const CU::Vector3f newPlayerPos = player->GetWorldPosition();
-	//
-	//	if((position - playerPos).Length2() > (position - newPlayerPos).Length2())
-	//	{
-	//		playerPos = newPlayerPos;
-	//	}
-	//}
-	//
-	//return playerPos;
-	return CU::Vector3f( 0,0,0 );
+	const CU::Vector3f position = GetParent()->GetWorldPosition();
+	
+	if(ourPlayerObjects.IsInitialized() == false || ourPlayerObjects.Size() == 0)
+	{
+		return position;
+	}
+	
+	CU::Vector3f playerPos = ourPlayerObjects[0]->GetWorldPosition();
+	for(int i = 0; i < ourPlayerObjects.Size(); ++i)
+	{
+		CGameObject*const player = ourPlayerObjects[i];
+		const CU::Vector3f newPlayerPos = player->GetWorldPosition();
+	
+		if((position - playerPos).Length2() > (position - newPlayerPos).Length2())
+		{
+			playerPos = newPlayerPos;
+		}
+	}
+	
+	return playerPos;
 }
--- Source/Components/Enemy.h
+++ Source/Components/Enemy.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "Component.h"
+#include "EnemyBlueprint.h"
 
 class CGameObject;
 
@@ -8,58 +9,39 @@ namespace Component
 	class CEnemy : public CComponent
 	{
 	public:
-		CEnemy(unsigned int anId);
+		CEnemy(unsigned int aId);
 		~CEnemy();
-		void SetHealth(const unsigned aHealth);
-		void SetSpeed(const float aSpeed);
-		void SetDetectionRange(const float aDetactionRange);
-		void SetStartAttackRange(const float aStartAttackRange);
-		void SetStopAttackRange(const float aStopAttackRange);
 
-		void Move(const CU::Vector3f& aDispl);
+		inline void SetEnemyData(const SEnemyBlueprint& aData);
+		static void SetPlayerObject(CGameObject* aPlayerObj);
+
 		void Update(const CU::Time& aDeltaTime);
-
 		void Receive(const eComponentMessageType aMessageType, const SComponentMessageData& aMessageData) override;
 
-		static void SetPlayer(CGameObject* aPlayerObject);
+	private:
+		CU::Vector3f ClosestPlayerPosition();
+		void UpdateTransformation();
+		void MoveForward(const float aMovAmount);
 
 	protected:
-		static CGameObject* myPlayerObj;
+		static CU::GrowingArray<CGameObject*> ourPlayerObjects;
 
-		CU::Vector3f ClosestPlayerPosition();
-		//static CU::GrowingArray<CGameObject*> myPlayerObject;
 		unsigned myHealth;
 		float mySpeed;
 		float myDetectionRange2;
 		float myStartAttackRange2;
 		float myStopAttackRange2;
+
 		bool myIsDead;
 		unsigned myServerId;
-
 	};
 }
 
-inline void Component::CEnemy::SetHealth(const unsigned aHealth)
+inline void Component::CEnemy::SetEnemyData(const SEnemyBlueprint& aData)
 {
-	myHealth = aHealth;
-}
-
-inline void Component::CEnemy::SetSpeed(const float aSpeed)
-{
-	mySpeed = aSpeed *100 ;
-}
-
-inline void Component::CEnemy::SetDetectionRange(const float aDetactionRange)
-{
-	myDetectionRange2 = aDetactionRange * aDetactionRange;
-}
-
-inline void Component::CEnemy::SetStartAttackRange(const float aStartAttackRange)
-{
-	myStartAttackRange2 = aStartAttackRange  * aStartAttackRange;
-}
-
-inline void Component::CEnemy::SetStopAttackRange(const float aStopAttackRange)
-{
-	myStopAttackRange2 = aStopAttackRange * aStopAttackRange;
+	myHealth = aData.health;
+	mySpeed = aData.health;
+	myDetectionRange2 = aData.detectionRange * aData.detectionRange;
+	myStartAttackRange2 = aData.startAttackRange * aData.startAttackRange;
+	myStopAttackRange2 = aData.stopAttackRange * aData.stopAttackRange;
 }
