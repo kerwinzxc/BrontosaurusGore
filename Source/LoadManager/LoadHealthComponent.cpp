815f9c929e3a75290d24ae45716def3659541617
Johan <johan.hagelqvist.sp15@thegameassembly.com>
2017-03-22 10:54:35 +0100

    bug fix

--- Source/LoadManager/LoadHealthComponent.cpp
+++ Source/LoadManager/LoadHealthComponent.cpp
@@ -9,10 +9,13 @@ int LoadHealthComponent(KLoader::SLoadedComponentData someData)
 	static unsigned int id = 0;
 	CHealthComponent* healthComponent = CHealthComponentManager::GetInstance()->CreateAndRegisterComponent(id++);
 	healthPoint loadedHealthValue = static_cast<healthPoint>(someData.myData.at("MaxHealth").GetInt());
-	armorPoint loadedArmorValue = static_cast<armorPoint>(someData.myData.at("MaxArmor").GetInt());
 	healthComponent->SetMaxHealth(loadedHealthValue);
 	healthComponent->SetHealth(loadedHealthValue);
-	healthComponent->SetMaxArmor(loadedArmorValue);
-	healthComponent->SetArmor(0);
+	if (someData.myData.HasKey("MaxArmor") == true)
+	{
+		armorPoint loadedArmorValue = static_cast<armorPoint>(someData.myData.at("MaxArmor").GetInt());
+		healthComponent->SetMaxArmor(loadedArmorValue);
+		healthComponent->SetArmor(0);
+	}
 	return healthComponent->GetId();
 }
