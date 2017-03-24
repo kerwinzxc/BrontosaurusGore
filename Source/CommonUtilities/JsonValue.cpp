a1f8e7fe3d123057a69dbcfb7de28c864aba923a
carlberyeur <carlcarlberger@gmail.com>
2017-03-24 11:12:35 +0100

    fixed bug in prettification of json value vectors made derived serializers' functions virtual so you can change how to save a matrix44 if its a transformation that doesn't use the last column for example

--- Source/CommonUtilities/JsonValue.cpp
+++ Source/CommonUtilities/JsonValue.cpp
@@ -316,7 +316,7 @@ namespace CU
 		}
 		assert(aNameXY.size() == 2u && "vector2 name does not match vector 2");
 
-		return Vector2f(at(&aNameXY[0]).GetFloat(), at(&aNameXY[1]).GetFloat());
+		return Vector2f(at(aNameXY.substr(0, 1)).GetFloat(), at(aNameXY.substr(1, 1)).GetFloat());
 	}
 	
 	Vector3f CJsonValue::GetVector3f(const std::string& aNameXYZ) const
@@ -339,7 +339,7 @@ namespace CU
 			return Vector3f();
 		}
 		assert(aNameXYZ.size() == 3u && "vector3 name does not match vector3");
-		return Vector3f(at(&aNameXYZ[0]).GetFloat(), at(&aNameXYZ[1]).GetFloat(), at(&aNameXYZ[2]).GetFloat());
+		return Vector3f(at(aNameXYZ.substr(0, 1)).GetFloat(), at(aNameXYZ.substr(1, 1)).GetFloat(), at(aNameXYZ.substr(2, 1)).GetFloat());
 	}
 
 	Vector4f CJsonValue::GetVector4f(const std::string& aNameXYZW) const
@@ -361,7 +361,7 @@ namespace CU
 		}
 		assert(aNameXYZW.size() == 4u && "vector4 name does not match vector 4");
 
-		return Vector4f(at(&aNameXYZW[0]).GetFloat(), at(&aNameXYZW[1]).GetFloat(), at(&aNameXYZW[2]).GetFloat(), at(&aNameXYZW[3]).GetFloat());
+		return Vector4f(at(aNameXYZW.substr(0, 1)).GetFloat(), at(aNameXYZW.substr(1, 1)).GetFloat(), at(aNameXYZW.substr(2, 1)).GetFloat(), at(aNameXYZW.substr(3, 1)).GetFloat());
 	}
 
 	CJsonValue CJsonValue::operator[](const int anIndex) const
--- Source/CommonUtilities/SerializerSaver.h
+++ Source/CommonUtilities/SerializerSaver.h
@@ -9,16 +9,16 @@ namespace CU
 		CSerilizerSaver(const unsigned int aBufferStartSize = 32);
 		~CSerilizerSaver();
 
-		void Cerealize(unsigned char& aChar) override;
-		void Cerealize(unsigned int& aUInt) override;
-		void Cerealize(float& aFloat) override;
-		void Cerealize(double& aDouble) override;
-		void Cerealize(CU::Vector2f& aVector2f) override;
-		void Cerealize(CU::Vector3f& aVector3f) override;
-		void Cerealize(CU::Vector4f& aVector4f) override;
-		void Cerealize(CU::Matrix33f& aMatrix33f) override;
-		void Cerealize(CU::Matrix44f& aMatrix44f) override;
-		void Cerealize(std::string& aString) override;
+		virtual void Cerealize(unsigned char& aChar) override;
+		virtual void Cerealize(unsigned int& aUInt) override;
+		virtual void Cerealize(float& aFloat) override;
+		virtual void Cerealize(double& aDouble) override;
+		virtual void Cerealize(CU::Vector2f& aVector2f) override;
+		virtual void Cerealize(CU::Vector3f& aVector3f) override;
+		virtual void Cerealize(CU::Vector4f& aVector4f) override;
+		virtual void Cerealize(CU::Matrix33f& aMatrix33f) override;
+		virtual void Cerealize(CU::Matrix44f& aMatrix44f) override;
+		virtual void Cerealize(std::string& aString) override;
 
 		const CU::GrowingArray<char>& GetBuffer() const;
 
