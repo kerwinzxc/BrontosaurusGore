566fcd46b5bc9027e7d80e3d4af1d9e230d3e1b3
Alex Pettersson <alpe0504@gmail.com>
2017-03-29 15:07:15 +0200

    Updated spawner for the new unity support

--- Source/Components/Spawner.cpp
+++ Source/Components/Spawner.cpp
@@ -1,11 +1,57 @@
 #include "stdafx.h"
 #include "Spawner.h"
 #include "EnemyFactory.h"
+#include "../Components/Enemy.h"
+#include "../ThreadedPostmaster/Postmaster.h"
+#include "../ThreadedPostmaster/SendNetowrkMessageMessage.h"
+#include "../TShared/NetworkMessage_SetIsRepesentationActive.h"
+#include "../TServer/ServerMessageManager.h"
 
-CSpawnerComponent::CSpawnerComponent(const unsigned char aId, const CU::TimeUnit aSpawnInterval)
+CSpawnerComponent::CSpawnerComponent(const CU::GrowingArray<unsigned char>& aWaves, const eEnemyTypes eEnemyType)
 {
-	mySpawnerId = aId;
-	mySpawInterval = aSpawnInterval;
+	mySpawInterval = 2.0f;
+	myWaves = aWaves;
+	switch (eEnemyType)
+	{
+	case eEnemyTypes::eImp:
+	{
+		SImpBlueprint aData;
+		aData.speed = 10.0f;
+		aData.jumpHeight = 4.0f;
+		aData.detectionRange = 30.0f;
+		aData.walkToMeleeRange = 0.f;
+		aData.startAttackRange = 0.0f;
+		aData.stopAttackRange = 1.0f;
+		myEnemy = CEnemyFactory::GetInstance()->CreateEnemy(&aData, eEnemyType, GetParent()->GetWorldPosition());
+	}
+		break;
+	case eEnemyTypes::ePinky:
+	{
+		SPinkyBlueprint aData;
+		aData.speed = 10.0f;
+		aData.detectionRange = 30.0f;
+		aData.walkToMeleeRange = 0.f;
+		aData.startAttackRange = 0.0f;
+		aData.stopAttackRange = 1.0f;
+		myEnemy = CEnemyFactory::GetInstance()->CreateEnemy(&aData, eEnemyType, GetParent()->GetWorldPosition());
+	}
+		break;
+	case eEnemyTypes::eRevenant:
+	{
+		SRevenantBlueprint aData;
+		aData.speed = 10.0f;
+		aData.detectionRange = 30.0f;
+		aData.walkToMeleeRange = 0.f;
+		aData.startAttackRange = 0.0f;
+		aData.stopAttackRange = 1.0f;
+		myEnemy = CEnemyFactory::GetInstance()->CreateEnemy(&aData, eEnemyType, GetParent()->GetWorldPosition());
+	}
+		break;
+	default:
+		break;
+	}
+
+	myEnemy->GetParent()->NotifyComponents(eComponentMessageType::eDeactivate, SComponentMessageData());
 }
 
 
@@ -27,12 +73,9 @@ void CSpawnerComponent::Update(const float aDeltaTime)
 
 void CSpawnerComponent::SpawnEnemy()
 {
-	SImpBlueprint aData;
-	aData.speed = 10.0f;
-	aData.jumpHeight = 4.0f;
-	aData.detectionRange = 30.0f;
-	aData.walkToMeleeRange = 0.f;
-	aData.startAttackRange = 0.0f;
-	aData.stopAttackRange = 1.0f;
-	CEnemyFactory::GetInstance()->CreateImp(&aData,GetParent()->GetWorldPosition());
+	myEnemy->GetParent()->NotifyComponents(eComponentMessageType::eActivate,SComponentMessageData());
+	CNetworkMessage_SetIsRepesentationActive* activeMessage = CServerMessageManager::GetInstance()->CreateMessage<CNetworkMessage_SetIsRepesentationActive>(ID_ALL);
+	activeMessage->SetActive(true);
+	activeMessage->SetNetworkID(myEnemy->GetNetworkID());
+	Postmaster::Threaded::CPostmaster::GetInstance().Broadcast(new CSendNetowrkMessageMessage(activeMessage));
 }
--- Source/Components/Spawner.h
+++ Source/Components/Spawner.h
@@ -1,14 +1,20 @@
 #pragma once
 #include "Component.h"
+#include "EnemyTypes.h"
+
 class CSpawnerComponent :
 	public CComponent
 {
 public:
-	CSpawnerComponent(const unsigned char aId, const CU::TimeUnit aSpawnInterval);
+	CSpawnerComponent(const CU::GrowingArray<unsigned char>& aWaves, const eEnemyTypes eEnemyType);
 	~CSpawnerComponent();
 	void Update(const float aDeltaTime);
 	void SpawnEnemy();
 private:
+	CU::GrowingArray<unsigned char> myWaves;
+
+	CEnemy* myEnemy;
+
 	unsigned char mySpawnerId;
 
 	CU::TimeUnit mySpawInterval;
