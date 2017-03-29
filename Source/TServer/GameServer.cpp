1bb99875a01bf252bcb97cf08c0ed05d06e028b9
Alex Pettersson <alpe0504@gmail.com>
2017-03-29 15:13:54 +0200

    stoped updateing the spawners

--- Source/TServer/GameServer.cpp
+++ Source/TServer/GameServer.cpp
@@ -173,7 +173,7 @@ bool CGameServer::Update(CU::Time aDeltaTime)
 	if(myTime > updateFrequecy)
 	{
 		myEnemyComponentManager->Update(aDeltaTime.GetSeconds() + (updateFrequecy / 1000.0f));
-		mySpawnerManager->Update(aDeltaTime.GetSeconds() + (updateFrequecy / 1000.0f));
+		//mySpawnerManager->Update(aDeltaTime.GetSeconds() + (updateFrequecy / 1000.0f));
 		myTime = 0;
 	}
 	
