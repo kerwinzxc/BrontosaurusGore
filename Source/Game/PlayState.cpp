18ec85298f6635db9761528b81916117ca304a4c
Legato <formerlym@outlook.com>
2017-03-30 09:52:35 +0200

    Made it so that clients won't start multiple local servers

--- Source/Game/PlayState.cpp
+++ Source/Game/PlayState.cpp
@@ -384,7 +384,7 @@ void CPlayState::CreateManagersAndFactories()
 void CPlayState::SpawnOtherPlayer(unsigned aPlayerID)
 {
 	CGameObject* otherPlayer = myGameObjectManager->CreateGameObject();
-	CModelComponent* model = myModelComponentManager->CreateComponent("Models/Meshes/M_BFG_01.fbx");
+	CModelComponent* model = myModelComponentManager->CreateComponent("Models/Meshes/M_Player_01.fbx");
 	CNetworkPlayerReciverComponent* playerReciver = new CNetworkPlayerReciverComponent;
 	playerReciver->SetPlayerID(aPlayerID);
 	CComponentManager::GetInstance().RegisterComponent(playerReciver);
