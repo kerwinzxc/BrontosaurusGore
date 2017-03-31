3340e9bc9271c9ae7d538c88263f942335c41ff5
Johan <johan.hagelqvist.sp15@thegameassembly.com>
2017-03-31 16:27:48 +0200

    utkommenterat för kompilering

--- Source/Components/WeaponPickupComponent.cpp
+++ Source/Components/WeaponPickupComponent.cpp
@@ -21,7 +21,10 @@ void CWeaponPickupComponent::SetWeaponPickup(const std::string& aWeaponName)
 
 void CWeaponPickupComponent::DoMyEffect()
 {
-	SComponentMessageData data;
-	data.myString = myWeaponPickup.c_str();
-	CPollingStation::GetInstance()->GetPlayerObject()->NotifyComponents(eComponentMessageType::eAddWeapon,data);
+	//SComponentMessageData data;
+	//data.myString = myWeaponPickup.c_str();
+	//CPollingStation::GetInstance()->GetPlayerObject()->NotifyComponents(eComponentMessageType::eAddWeapon,data);
+	//CNetworkMessage_PickupWeapon* message = CClientMessageManager::GetInstance()->CreateMessage<CNetworkMessage_PickupWeapon>(ID_ALL_BUT_ME);
+	//message->SetID(myNetworkId);
+	//Postmaster::Threaded::CPostmaster::GetInstance().Broadcast(new CSendNetworkMessageMessage(message));
 }
