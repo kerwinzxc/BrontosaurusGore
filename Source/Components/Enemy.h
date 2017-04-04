54f0c0f636b265ee108cfea63140ed38d09e385b
Alex Pettersson <alpe0504@gmail.com>
2017-04-04 09:48:49 +0200

    Enemy now can be set to ignore being part of the checkoint system

--- Source/Components/Enemy.h
+++ Source/Components/Enemy.h
@@ -20,6 +20,7 @@ public:
 	virtual void Update(const float aDeltaTime) = 0;
 	void Receive(const eComponentMessageType aMessageType, const SComponentMessageData& aMessageData) override;
 	bool Answer(const eComponentQuestionType aQuestionType, SComponentQuestionData& aQuestionData) override;
+	void KillEverythingThenResetItAgain(const bool aFlag);
 	virtual	void ChangeWeapon(const unsigned int aIndex);
 	inline eEnemyTypes GetEnemyType();
 	inline void SetType(const eEnemyTypes aType);
@@ -64,6 +65,7 @@ protected:
 	eEnemyTypes myType;
 	bool myIsDead;
 	bool myIsAttacking;
+	bool myShouldNotReset;
 	char myControllerConstraints;
 };
 
