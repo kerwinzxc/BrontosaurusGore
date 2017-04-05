5de69241ee58e0156a3bc5b4df798c7879bdf27b
MarcusM <marcus.malmqvist.sp15@thegameassembly.com>
2017-04-05 17:18:24 +0200

    EnemyRunTowardsComponent(bad name) gets added now to enemies

--- Source/LoadManager/LoadEnemyRunTowardsComponent.cpp
+++ Source/LoadManager/LoadEnemyRunTowardsComponent.cpp
@@ -1,11 +1,13 @@
 #include "stdafx.h"
 #include "LoadEnemyRunTowardsComponent.h"
 #include "EnemyRunTowardsComponent.h"
+#include "Enemy.h"
 
 int LoadEnemyRunTowardsComponent(KLoader::SLoadedComponentData someData)
 {
 		CEnemyRunTowardsComponent* newEnemyRunTowardsComponent = new CEnemyRunTowardsComponent();
 		CComponentManager::GetInstance().RegisterComponent(newEnemyRunTowardsComponent);
+		CEnemy::AddEnemyRunTowardsComponent(newEnemyRunTowardsComponent);
 		return newEnemyRunTowardsComponent->GetId();
 
 	return NULL_COMPONENT;
