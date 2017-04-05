47aa30140f1d48a01c8be76c2f1b21130b40c52a
MarcusM <marcus.malmqvist.sp15@thegameassembly.com>
2017-04-05 15:27:34 +0200

    Enemies should die now if they fall off the map

--- Source/Components/EnemyClientRepresentation.h
+++ Source/Components/EnemyClientRepresentation.h
@@ -15,6 +15,7 @@ public:
 	inline const eEnemyTypes GetEnemyType() const;
 	inline void SetEnemyType(const eEnemyTypes aType);
 	void Receive(const eComponentMessageType aMessageType, const SComponentMessageData& aMessageData) override;
+	void CheckIfOutOfBounds();
 protected:
 	unsigned myComponentId;
 	CU::Matrix44f myFutureMatrix;
