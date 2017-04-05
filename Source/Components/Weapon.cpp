af00bdab9b228ececf8e11c48a748f275b0c98d6
MarcusM <marcus.malmqvist.sp15@thegameassembly.com>
2017-04-05 10:35:19 +0200

    I'm dumb :sob:

--- Source/Components/Weapon.cpp
+++ Source/Components/Weapon.cpp
@@ -88,9 +88,6 @@ void CWeapon::Shoot(const CU::Vector3f& aDirection)
 						SComponentMessageData damageData;
 						damageData.myInt = myWeaponData->projectileData->damage;
 						gameObject->NotifyComponents(eComponentMessageType::eTakeDamage, damageData);
-						direction = gameObject->GetWorldPosition() - shootPosition;
-						direction.Normalize();
-						//Do massive Domage!!
 					
 					}
 				}
