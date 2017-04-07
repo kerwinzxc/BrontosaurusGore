cedcd97d6e35b23c7af78a559ebfa01d59b833ac
Alex Pettersson <alpe0504@gmail.com>
2017-04-07 14:47:02 +0200

    Enemy reps now gets a more correct first position

--- Source/TShared/NetworkMessage_SpawnEnemyRepesention.cpp
+++ Source/TShared/NetworkMessage_SpawnEnemyRepesention.cpp
@@ -26,6 +26,11 @@ void CNetworkMessage_SpawnEnemyRepesention::SetEnemyType(const eEnemyTypes aType
 	myEnemyType = aType;
 }
 
+void CNetworkMessage_SpawnEnemyRepesention::SetPosition(const CU::Vector3f& aPosition)
+{
+	myPosition = aPosition;
+}
+
 const short CNetworkMessage_SpawnEnemyRepesention::GetHealth() const
 {
 	return myEnemyLife;
@@ -36,11 +41,17 @@ const eEnemyTypes CNetworkMessage_SpawnEnemyRepesention::GetEnemyType() const
 	return myEnemyType;
 }
 
+const CU::Vector3f  CNetworkMessage_SpawnEnemyRepesention::GetPosition() const
+{
+	return myPosition;
+}
+
 void CNetworkMessage_SpawnEnemyRepesention::DoSerialize(StreamType & aStream)
 {
 	CImportantNetworkMessage::DoSerialize(aStream);
 	serialize(myEnemyLife, aStream);
 	serialize(myEnemyType, aStream);
+	serialize(myPosition, aStream);
 }
 
 void CNetworkMessage_SpawnEnemyRepesention::DoDeserialize(StreamType & aStream)
@@ -48,4 +59,5 @@ void CNetworkMessage_SpawnEnemyRepesention::DoDeserialize(StreamType & aStream)
 	CImportantNetworkMessage::DoDeserialize(aStream);
 	DESERIALIZE(myEnemyLife, aStream);
 	DESERIALIZE(myEnemyType, aStream);
+	DESERIALIZE(myPosition, aStream);
 }
