26fc4b956e541ede8f6cc54753774f9948b11f2a
carlberyeur <carlcarlberger@gmail.com>
2017-04-10 15:12:57 +0200

    made my function class tihihihihi

--- Source/CommonUtilities/Function.h
+++ Source/CommonUtilities/Function.h
@@ -1,113 +1,137 @@
 #pragma once
 
-#define BASIC_FUNC_PTR_SIZE sizeof(int*)
-#define self (*this)
-
 #include <functional>
 
-template<bool IS_BIG, int FUNCTION_SIZE, typename RETURN_TYPE, typename... ARGS>
-struct SFunctionImpl
+template<typename Ret, typename... Arg>
+class IBaseFunction
 {
-	using ImplType = RETURN_TYPE(*)(ARGS...);
+public:
+	IBaseFunction() {}
+	virtual ~IBaseFunction() {}
 
-	SFunctionImpl() { memset(this, 0, sizeof(*this)); }
-	~SFunctionImpl() {}
-
-	ImplType Get() { return reinterpret_cast<ImplType>(&myImpl[0]); }
-	void Set(const ImplType& aFuncPtr) { Get() = aFuncPtr; }
-	
-	char myImpl[FUNCTION_SIZE];
+	virtual Ret Call(Arg&&... aArgs) = 0;
+	virtual unsigned int SizeOf() = 0;
 };
 
-template<int FUNCTION_SIZE, typename RETURN_TYPE, typename... ARGS>
-struct SFunctionImpl<true, FUNCTION_SIZE, RETURN_TYPE, ARGS...>
+template<typename T, typename Ret, typename... Arg>
+class CEraserFunction : public IBaseFunction<Ret, Arg...>
 {
-	using ImplType = RETURN_TYPE(*)(ARGS...);
+public:
+	CEraserFunction(T aFunctionObject)
+		: myFunctionObject(aFunctionObject)
+	{
+	}
 
-	SFunctionImpl() { myImpl = new char[FUNCTION_SIZE]; memset(myImpl, 0, FUNCTION_SIZE); }
-	~SFunctionImpl() { delete[] myImpl; }
+	~CEraserFunction()
+	{
+	}
 
-	ImplType Get() { return static_cast<ImplType>(myImpl); }
-	void Set(const ImplType& aFuncPtr) { memcpy(myImpl, aFuncPtr, FUNCTION_SIZE); }
+	virtual Ret Call(Arg&&... aArgs)
+	{
+		return myFunctionObject(std::forward<Arg>(aArgs)...);
+	}
 
-	char* myImpl;
+	virtual unsigned int SizeOf()
+	{
+		return sizeof(*this);
+	}
+
+private:
+	T myFunctionObject;
 };
 
-template<typename ReturnType, typename... Arguments>
+template<typename Ret, typename... Arg>
 class CFunction
 {
 public:
-	CFunction();
-	CFunction(const CFunction& aCopy);
-	template<typename LAMBDA> CFunction(LAMBDA aLambda);
-	~CFunction();
+	CFunction()
+		: myFunctionObject(nullptr)
+	{
+	}
 
-	inline ReturnType operator()(Arguments... aArguments);
+	~CFunction()
+	{
+		Destroy();
+	}
 
-	inline CFunction& operator=(const CFunction& aCopy);
-	template<typename LAMBDA> inline CFunction& operator=(LAMBDA aLambda);
+	template<typename T>
+	CFunction(T aFunctionObject)
+	{
+		Create<T>(aFunctionObject);
+	}
 
-	inline bool operator==(const CFunction& aOther) const;
-	inline bool operator==(const std::nullptr_t aPointer) const;
+	template<typename T>
+	CFunction& operator=(T aFunctionObject)
+	{
+		Destroy();
+		Create<T>(aFunctionObject);
+		return *this;
+	}
+
+	CFunction& operator=(std::nullptr_t)
+	{
+		Destroy();
+		return *this;
+	}
+
+	Ret operator()(Arg... aArgs)
+	{
+		assert(myFunctionObject);
+		if (myFunctionObject)
+		{
+			return myFunctionObject->Call(std::forward<Arg>(aArgs)...);
+		}
+
+		return Ret();
+	}
+
+	bool operator==(const CFunction& aOther) const
+	{
+		return myFunctionObject == aOther.myFunctionObject;
+	}
+
+	bool operator==(std::nullptr_t) const
+	{
+		return !myFunctionObject;
+	}
+
+	bool operator!() const
+	{
+		return !myFunctionObject;
+	}
+
+	operator bool() const
+	{
+		return myFunctionObject != nullptr;
+	}
 
 private:
-	SFunctionImpl<false, BASIC_FUNC_PTR_SIZE, ReturnType, Arguments...> myImpl;
+	template<typename T>
+	void Create(T aFunctionObject)
+	{
+		if /*constexpr*/ (sizeof(T) <= sizeof(void*))
+		{
+			myFunctionObject = new (myMemoryBlob) CEraserFunction<T, Ret, Arg...>(aFunctionObject);
+		}
+		else
+		{
+			myFunctionObject = new CEraserFunction<T, Ret, Arg...>(aFunctionObject);
+		}
+	}
+
+	void Destroy()
+	{
+		if (myFunctionObject->SizeOf() <= sizeof(void*))
+		{
+			myFunctionObject->~IBaseFunction();
+			myFunctionObject = nullptr;
+		}
+		else
+		{
+			SAFE_DELETE(myFunctionObject);
+		}
+	}
+
+	IBaseFunction<Ret, Arg...>* myFunctionObject;
+	char myMemoryBlob[sizeof(Ret(*)(Arg...))];
 };
-
-template<typename ReturnType, typename... Arguments>
-CFunction<ReturnType, Arguments...>::CFunction()
-{
-}
-
-template<typename ReturnType, typename ...Arguments>
-CFunction<ReturnType, Arguments...>::CFunction(const CFunction& aCopy)
-{
-	self = aCopy;
-}
-
-template<typename ReturnType, typename ...Arguments>
-template<typename LAMBDA>
-inline CFunction<ReturnType, Arguments...>::CFunction(LAMBDA aLambda)
-{
-	self = aLambda;
-}
-
-template<typename ReturnType, typename ...Arguments>
-CFunction<ReturnType, Arguments...>::~CFunction()
-{
-}
-
-template<typename ReturnType, typename ...Arguments>
-inline ReturnType CFunction<ReturnType, Arguments...>::operator()(Arguments... aArguments)
-{
-	return myImpl.Get()(aArguments...);
-}
-
-template<typename ReturnType, typename ...Arguments>
-inline CFunction<ReturnType, Arguments...>& CFunction<ReturnType, Arguments...>::operator=(const CFunction& aCopy)
-{
-	myImpl = aCopy.myImpl;
-	return self;
-}
-
-template<typename ReturnType, typename ...Arguments>
-template<typename LAMBDA>
-inline CFunction<ReturnType, Arguments...>& CFunction<ReturnType, Arguments...>::operator=(LAMBDA aLambda)
-{
-	myImpl = SFunctionImpl<(sizeof(LAMBDA) > 8), sizeof(LAMBDA), ReturnType, Args...>();
-	return self;
-}
-
-template<typename ReturnType, typename ...Arguments>
-inline bool CFunction<ReturnType, Arguments...>::operator==(const CFunction& aOther) const
-{
-	return myImpl.myImpl == aOther.myImpl.myImpl;
-}
-
-template<typename ReturnType, typename ...Arguments>
-inline bool CFunction<ReturnType, Arguments...>::operator==(const std::nullptr_t aPointer) const
-{
-	return myImpl.myImpl == aPointer;
-}
-
-#undef self
