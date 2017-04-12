d5f80b07607e4303caefaa3c4b0f6639d8e46eb5
HaNaK0 <saturn-v@hotmail.com>
2017-04-12 18:05:50 +0200

    More menu features

--- Source/Game/MenuState.cpp
+++ Source/Game/MenuState.cpp
@@ -11,11 +11,27 @@
 #include "RenderMessages.h"
 #include "Engine.h"
 #include "Renderer.h"
+#include <Shlwapi.h>
+#include "PlayState.h"
+#include "LoadState.h"
+#include "WindowsHelper.h"
+#include "WindowsWindow.h"
+#include "ThreadedPostmaster/ConetctMessage.h"
+#include "TShared/NetworkMessage_LoadLevel.h"
+#include "TClient/ClientMessageManager.h"
+#include "ThreadedPostmaster/SendNetowrkMessageMessage.h"
 
-CMenuState::CMenuState(StateStack& aStateStack, std::string aFile): State(aStateStack, eInputMessengerType::eMainMenu), myShowStateBelow(false), myPointerSprite(nullptr)
+char CMenuState::ourMenuesToPop = 0;
+
+CMenuState::CMenuState(StateStack& aStateStack, std::string aFile): State(aStateStack, eInputMessengerType::eMainMenu), myShowStateBelow(false), myPointerSprite(nullptr),myIsInFocus(false)
 {
 	myManager.AddAction("ExitGame", bind(&CMenuState::ExitGame, std::placeholders::_1));
 	myManager.AddAction("PushTempLobby", bind(&CMenuState::PushTempLobby, this, std::placeholders::_1));
+	myManager.AddAction("PushMenu", bind(&CMenuState::PushMenu, this, std::placeholders::_1));
+	myManager.AddAction("PopMenues", bind(&CMenuState::PopMenues, this, std::placeholders::_1));
+	myManager.AddAction("PushLevel", bind(&CMenuState::PushLevel, this, std::placeholders::_1));
+	myManager.AddAction("StartServer", bind(&CMenuState::StartServer, std::placeholders::_1));
+	myManager.AddAction("ConectLocal", bind(&CMenuState::ConnectLocal, std::placeholders::_1));
 
 	MenuLoad(aFile);
 }
@@ -31,7 +47,15 @@ void CMenuState::Init()
 eStateStatus CMenuState::Update(const CU::Time& aDeltaTime)
 {
 	myManager.Update(aDeltaTime);
-	return eStateStatus::eKeep;
+	if (ourMenuesToPop > 0)
+	{
+		ourMenuesToPop -= 1;
+		return eStateStatus::ePop;
+	}
+	else
+	{
+		return eStateStatus::eKeep;
+	}
 }
 
 void CMenuState::Render()
@@ -41,15 +65,21 @@ void CMenuState::Render()
 
 void CMenuState::OnEnter(const bool aLetThroughRender)
 {
+	myIsInFocus = true;
 }
 
 void CMenuState::OnExit(const bool aLetThroughRender)
 {
 	RENDERER.ClearGui();
+	myIsInFocus = false;
 }
 
 CU::eInputReturn CMenuState::RecieveInput(const CU::SInputMessage& aInputMessage)
 {
+	if (myIsInFocus == false)
+	{
+		return CU::eInputReturn::ePassOn;
+	}
 	switch (aInputMessage.myType)
 	{
 	case CU::eInputType::eMouseMoved: 
@@ -73,7 +103,7 @@ CU::eInputReturn CMenuState::RecieveInput(const CU::SInputMessage& aInputMessage
 	default: break;
 	}
 
-	return CU::eInputReturn::ePassOn;
+	return CU::eInputReturn::eKeepSecret;
 }
 
 eAlignment CMenuState::LoadAlignment(const CU::CJsonValue& aJsonValue)
@@ -154,6 +184,11 @@ void CMenuState::MenuLoad(const std::string& aFile)
 	}
 }
 
+void CMenuState::PushMenu(std::string aMenu) const
+{
+	myStateStack.PushState(new CMenuState(myStateStack, aMenu));
+}
+
 void CMenuState::ExitGame(std::string /* not used*/)
 {
 	Postmaster::Threaded::CPostmaster::GetInstance().Broadcast(new CQuitGame);
@@ -161,6 +196,44 @@ void CMenuState::ExitGame(std::string /* not used*/)
 
 void CMenuState::PushTempLobby(std::string /*notUsed*/) const
 {
-
 	myStateStack.PushState(new CTempLobbyState(myStateStack)); 
+}
+
+void CMenuState::PopMenues(std::string aNumberOfMenues) const
+{
+	ourMenuesToPop = stoi(aNumberOfMenues);
+}
+
+void CMenuState::PushLevel(std::string aLevelIndexString) const
+{
+	myStateStack.PushState(new CLoadState(myStateStack, std::stoi(aLevelIndexString)));
+	CNetworkMessage_LoadLevel* netowrkMessageMessage = CClientMessageManager::GetInstance()->CreateMessage<CNetworkMessage_LoadLevel>("__All_But_Me");
+	netowrkMessageMessage->myLevelIndex = std::stoi(aLevelIndexString);
+	Postmaster::Threaded::CPostmaster::GetInstance().Broadcast(new CSendNetworkMessageMessage(netowrkMessageMessage));
+}
+
+void CMenuState::StartServer(std::string /*notUsed*/)
+{
+	std::string processName = "TServer_Applictaion_x64_";
+
+#ifdef _DEBUG
+	processName += "Debug";
+#elif defined(RETAIL)
+	processName += "Retail";
+#elif defined(RELEASE)
+	processName += "Release";
+#endif
+
+	processName += ".exe";
+	if (!WindowsHelper::StartProgram(processName))
+	{
+		DL_MESSAGE_BOX("Failed to start server, failed to build TServer_Application?\n%s", processName.c_str());
+	}
+	std::this_thread::sleep_for(std::chrono::milliseconds(500));
+	WindowsHelper::SetFocus(CEngine::GetInstance()->GetWindow()->GetHWND());
+}
+
+void CMenuState::ConnectLocal(std::string anIp)
+{
+	Postmaster::Threaded::CPostmaster::GetInstance().Broadcast(new CConectMessage("YouAreAlone", "127.0.0.1"));
 }
