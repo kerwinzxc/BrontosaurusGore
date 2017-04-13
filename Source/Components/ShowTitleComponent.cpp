9d7938f768ab4a361994d15887eca639f950066c
Edvin Jacobsson Pontén <resanderum@gmail.com>
2017-04-13 18:06:13 +0200

    added a stupid title component

---
+++ Source/Components/ShowTitleComponent.cpp
@@ -0,0 +1,48 @@
+#include "stdafx.h"
+#include "ShowTitleComponent.h"
+#include "ComponentManager.h"
+
+CShowTitleComponent::CShowTitleComponent()
+{
+	if (CComponentManager::GetInstancePtr())
+	{
+		CComponentManager::GetInstance().RegisterComponent(this);
+	}
+	myAlpha = 0.0f;
+	myFadeIn = false;
+}
+
+
+CShowTitleComponent::~CShowTitleComponent()
+{
+}
+
+
+void CShowTitleComponent::Update(const float aDT)
+{
+	if (myFadeIn)
+	{
+		if(myAlpha < 1.0f)
+			myAlpha += 3.5 * aDT;
+	}
+	else
+	{
+		if (myAlpha > 0.0f)
+			myAlpha -= 1.5 * aDT;
+	}
+	CLAMP(myAlpha, 0.0f, 1.0f);
+}
+
+void CShowTitleComponent::Receive(const eComponentMessageType aMessageType, const SComponentMessageData& aMessageData)
+{
+	switch (aMessageType)
+	{
+	case eComponentMessageType::eOnTriggerEnter:
+		myFadeIn = true;
+		break;
+	case eComponentMessageType::eOnTriggerExit:
+		myFadeIn = false;
+		break;
+	}
+}
+
