bbdf3b08c29eb15fc23cb91ac6f3b2098449a4ed
Alex Pettersson <alpe0504@gmail.com>
2017-04-13 18:16:44 +0200

    scale

--- Source/Components/ProjectileFactory.cpp
+++ Source/Components/ProjectileFactory.cpp
@@ -89,7 +89,7 @@ void CProjectileFactory::Init(CGameObjectManager* aGameObjectManager, CModelComp
 void CProjectileFactory::CreateProjectile(unsigned int aIndex)
 {
 	CGameObject* newProjectileObject = myGameObjectManagerPointer->CreateGameObject();
-	newProjectileObject->GetLocalTransform().Scale(CU::Vector3f(1, 1, 1));
+	newProjectileObject->GetLocalTransform().Scale(CU::Vector3f(0.1f, 0.1f, 0.1f));
 	CProjectileComponent* tempProjectileComponent = myProjectileComponentManager->CreateAndRegisterComponent();
 	newProjectileObject->AddComponent(tempProjectileComponent);
 	if (!myPassiveProjectiles[aIndex]->projectileName.empty())
